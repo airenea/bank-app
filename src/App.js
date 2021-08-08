63c430e6f5dd698391e1aae0d0014353087ff907
Airene Maria <aireneapacible@gmail.com>
2021-08-08 18:05:50 +0800

    Starting up bank app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Hello, world.
+            Bank app in progress!
         </p>
         <a
           className="App-link"
